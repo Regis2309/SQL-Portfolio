da0534871ae9af78fd9824c329748578c05c25e5
Regis2309 <primoregis@yahoo.ca>
2022-01-14 11:37:18 -0500

    14 01 2022

--- Technology Adoption - Technology Adoption by household.sql
+++ Technology Adoption - Technology Adoption by household.sql
@@ -1,1 +1,5 @@
+USE [Technology adoption by household in the US]
+Select * From ['Technology-Adoption-By-Househol$']
+ALTER TABLE ['Technology-Adoption-By-Househol$']
+DROP COLUMN Code
 Select * From ['Technology-Adoption-By-Househol$']
